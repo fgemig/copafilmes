abe80709e2a45e7e3ba4acb2519575c26f06964a
Fabio Gemignani <fabiogemignani@outlook.com>
2020-10-26 21:35:52 -0300

    Criando classes Filme e Partida

---
+++ src/CopaFilmes.Api/Models/Filme.cs
@@ -0,0 +1,24 @@
+﻿namespace CopaFilmes.Api.Models
+{
+    /// <summary>
+    /// Cria a instância de um filme
+    /// </summary>
+    public class Filme 
+    {
+        public Filme(string id, string titulo, int ano, decimal nota)
+        {
+            Id = id;
+            Titulo = titulo;
+            Ano = ano;
+            Nota = nota;
+        }
+
+        public string Id { get; private set; }
+
+        public string Titulo { get; private set; }
+
+        public int Ano { get; private set; }
+
+        public decimal Nota { get; private set; }
+    }
+}
---
+++ src/CopaFilmes.Api/Models/Partida.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CopaFilmes.Api.Models
+{
+    /// <summary>
+    /// Cria a instância de uma partida e obtém o filme vencedor
+    /// </summary>
+    public class Partida
+    {
+        public Partida(Filme filmeA, Filme filmeB)
+        {
+            FilmeA = filmeA ?? throw new ArgumentNullException(nameof(filmeA));
+            FilmeB = filmeB ?? throw new ArgumentNullException(nameof(filmeB));
+        }
+
+        public Filme FilmeA { get; }
+
+        public Filme FilmeB { get; }
+
+        /// <summary>
+        /// Obtém o filme vencedor por critério de nota. Caso as notas sejam iguais, o vencedor será definido por ordem alfabética.
+        /// </summary>
+        /// <returns></returns>
+        public Filme FilmeVencedor()
+        {
+            if (FilmeA.Nota > FilmeB.Nota)
+                return FilmeA;
+
+            if (FilmeB.Nota > FilmeA.Nota)
+                return FilmeB;
+
+            return VencedorPorCriterioDeDesempate();
+        }
+
+        private Filme VencedorPorCriterioDeDesempate()
+        {
+            var lista = new List<Filme>() { FilmeA, FilmeB };
+
+            return lista.OrderBy(c => c.Titulo).First();
+        }
+    }
+}
