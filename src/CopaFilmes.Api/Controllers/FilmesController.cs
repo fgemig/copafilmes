990e3d843707af386a1f2e3ae1d5199b50d09da6
Fabio Gemignani <fabiogemignani@outlook.com>
2020-10-27 22:40:27 -0300

    Criando Controller para listagem dos filmes da api CopaFilmes

---
+++ src/CopaFilmes.Api/Controllers/FilmesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using CopaFilmes.Api.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CopaFilmes.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmesController : ControllerBase
+    {
+        private readonly IRepositorioDeFilmes _repositorioDeFilmes;
+        private readonly ILogger<FilmesController> _logger;
+
+        public FilmesController(
+            IRepositorioDeFilmes repositorioDeFilmes,
+            ILogger<FilmesController> logger)
+        {
+            _repositorioDeFilmes = repositorioDeFilmes;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var filmes = await _repositorioDeFilmes.ObterFilmes();
+                return Ok(filmes);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "Falha ao consultar a API CopaFilmes");
+
+                return StatusCode(500);
+            }          
+        }
+    }
+}
