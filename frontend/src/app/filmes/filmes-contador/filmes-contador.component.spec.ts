e26991b808ea2109061905b58917421b08bdf9be
Fabio Gemignani <fabiogemignani@outlook.com>
2020-10-31 21:28:24 -0300

    Adicionando testes para o componente Contador de Filmes

--- frontend/src/app/filmes/filmes-contador/filmes-contador.component.spec.ts
+++ frontend/src/app/filmes/filmes-contador/filmes-contador.component.spec.ts
@@ -1,27 +1,59 @@
-import { HttpClientModule } from '@angular/common/http';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
+import { Filme } from '../filme';
+import { FilmesService } from '../filmes.service';
 import { FilmesContadorComponent } from './filmes-contador.component';
 
+const filmes: Filme[] = [
+  {
+    id: '00001',
+    titulo: 'Filme 1',
+    ano: 2020,
+    nota: 5.5
+  }
+];
+
 describe('FilmesContadorComponent', () => {
+
   let component: FilmesContadorComponent;
   let fixture: ComponentFixture<FilmesContadorComponent>;
+  let filmesService: FilmesService;
 
-  beforeEach(async(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
-      declarations: [ FilmesContadorComponent ],
-      imports: [HttpClientModule]
+      declarations: [FilmesContadorComponent],
+      providers: [
+        {
+          provide: FilmesService, useValue: {
+            obterTotalSelecionado: () => of(filmes)
+          }
+        }
+      ]
     })
-    .compileComponents();
-  }));
+      .compileComponents();
+  });
 
-  beforeEach(() => {
+  beforeEach(async () => {
     fixture = TestBed.createComponent(FilmesContadorComponent);
     component = fixture.componentInstance;
+    filmesService = TestBed.inject(FilmesService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('deve receber uma lista de filmes selecionados', () => {
+    spyOn(filmesService, 'obterTotalSelecionado')
+      .and
+      .callThrough();
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(filmesService.obterTotalSelecionado);
+    expect(component.filmesSelecionados.length).toEqual(1);
+  });
+
+  it('deve mostrar a quantidade de filmes selecionados"', () => {
+    const contadorElemento: HTMLElement = fixture.nativeElement;
+    const totalSelecionadoDiv = contadorElemento.querySelector('.total-selecionado');
+    expect(totalSelecionadoDiv.textContent).toContain('1 de 8 filmes');
   });
 });
