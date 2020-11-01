ef3b807614b59becbd2bb97cfe7476ada0b86398
Fabio Gemignani <fabiogemignani@outlook.com>
2020-10-31 21:30:10 -0300

    Adicionando testes para o componente de listagem de filmes

--- frontend/src/app/filmes/filmes-lista/filmes-lista.component.spec.ts
+++ frontend/src/app/filmes/filmes-lista/filmes-lista.component.spec.ts
@@ -1,27 +1,56 @@
-import { HttpClientModule } from '@angular/common/http';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
+import { Filme } from '../filme';
+import { FilmesService } from './../filmes.service';
 import { FilmesListaComponent } from './filmes-lista.component';
 
+const filmes: Filme[] = [
+  {
+    id: '00001',
+    titulo: 'Filme 1',
+    ano: 2020,
+    nota: 5.5
+  }
+];
+
 describe('FilmesListaComponent', () => {
+
   let component: FilmesListaComponent;
   let fixture: ComponentFixture<FilmesListaComponent>;
+  let filmesService: FilmesService;
 
-  beforeEach(async(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
-      declarations: [ FilmesListaComponent ],
-      imports: [HttpClientModule]
+      declarations: [FilmesListaComponent],
+      providers: [
+        {
+          provide: FilmesService, useValue: {
+            obterFilmes: () => of(filmes)
+          }
+        }
+      ]
     })
-    .compileComponents();
-  }));
+      .compileComponents();
+  });
 
-  beforeEach(() => {
+  beforeEach(async () => {
     fixture = TestBed.createComponent(FilmesListaComponent);
     component = fixture.componentInstance;
+    filmesService = TestBed.inject(FilmesService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('deve carregar uma lista de filmes', () => {
+    spyOn(filmesService, 'obterFilmes')
+    .and
+    .callThrough();
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(filmesService.obterFilmes);
+
+    component.filmes$.subscribe(c => {
+      expect(c.length).toEqual(1);
+    });
   });
 });
