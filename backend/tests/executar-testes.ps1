19af4b9799512621dc446dabbd4ce466a149ba2d
Fabio Gemignani <fabiogemignani@outlook.com>
2020-11-01 23:35:31 -0300

    Configurando coverlet/reportgenerator para gerar relatório de cobertura de testes

---
+++ backend/tests/executar-testes.ps1
@@ -0,0 +1,8 @@
+﻿ 
+dotnet test CopaFilmes.Testes.Integracao\CopaFilmes.Testes.Integracao.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=CopaFilmes.Testes.Integracao\relatorio-cobertura\coverage.opencover.xml
+reportgenerator -reports:CopaFilmes.Testes.Integracao\relatorio-cobertura\coverage.opencover.xml -targetdir:CopaFilmes.Testes.Integracao\relatorio-cobertura
+
+dotnet test CopaFilmes.Testes.Unidade\CopaFilmes.Testes.Unidade.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=CopaFilmes.Testes.Unidade\relatorio-cobertura\coverage.opencover.xml
+reportgenerator -reports:CopaFilmes.Testes.Unidade\relatorio-cobertura\coverage.opencover.xml -targetdir:CopaFilmes.Testes.Unidade\relatorio-cobertura
+
+pause
