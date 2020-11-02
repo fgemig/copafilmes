1c5a258a0a4077160a5444e616e6b0899368f957
Fabio Gemignani <fabiogemignani@outlook.com>
2020-11-02 12:43:42 -0300

    Adicionando biblioteca polly para resiliência nas chamadas http

--- backend/src/CopaFilmes.Api/Extensoes/ConfiguracoesDI.cs
+++ backend/src/CopaFilmes.Api/Extensoes/ConfiguracoesDI.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using CopaFilmes.Api.Models;
+using System.Net.Http;
+using Polly;
 
 namespace CopaFilmes.Api.Extensoes
 {
@@ -11,10 +13,17 @@ namespace CopaFilmes.Api.Extensoes
     {
         public static void AddDependencyInjection(this IServiceCollection services, IConfiguration configuration)
         {
+            var politicaDeRetentativas = Policy
+                .HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+                .RetryAsync(2, onRetry: (message, tentativa) =>
+                {
+                    Console.Out.WriteLineAsync($"Retentativa: {tentativa}");
+                });
+
             services.AddHttpClient<IRepositorioDeFilmes, RepositorioDeFilmes>(client =>
             {
                 client.BaseAddress = new Uri(configuration["ParametrosApi:UrlApiCopaFilmes"]);
-            });
+            }).AddPolicyHandler(politicaDeRetentativas);
 
             services.AddScoped<IGerenciadorDePartidas, GerenciadorDePartidas>();
             services.AddScoped<IGerenciadorDeCampeonato, GerenciadorDeCampeonato>();
